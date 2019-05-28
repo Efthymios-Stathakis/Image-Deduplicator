794389b9fe61c942b3cf4c5b076392c39003640f
Efthymios <efstatha@hotmail.com>
2019-05-28 10:09:08 +0200

    Update HashDeduplicator.py
    Refactored the code

--- Hash/HashDeduplicator.py
+++ Hash/HashDeduplicator.py
@@ -93,27 +93,3 @@ class HashDeduplicator():
     @staticmethod
     def hamming_dist(input_arr, input_vec):
         return np.abs(input_arr - input_vec.reshape(-1,1)).mean(axis=0)
-
-def run():
-    print('Starting Deduplicator')
-    
-    with open('config.json') as json_file:  
-        params = json.load(json_file)
-        
-    path_params     = params['relative_path']
-    image_deduplicator = ImageDeduplicator(path_params)
-    
-    lof_images = params['lof_images']
-    check_all  = params['check_all']
-    if check_all == 'Yes':
-        lof_images = image_deduplicator._lof_filenames
-    
-    for img in lof_images:
-        lof_duplicates = image_deduplicator._find_duplicates(img)
-        if len(lof_duplicates)==0:
-            print('Found no duplicates for {}'.format(img))
-        else:
-            print('Found {} as duplicates for {}'.format(lof_duplicates, img))
-            
-if __name__ == "__main__":
-    run() 
